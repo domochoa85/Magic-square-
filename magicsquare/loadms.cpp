962ec66c1f2444527cb192e72fd80d47062c380d
zhangsgithub04 <autodsappt@instance-344.us-central1-a.c.csci2243.internal>
2022-11-15 16:57:23 +0000

    load square

--- magicsquare/loadms.cpp
+++ magicsquare/loadms.cpp
@@ -3,35 +3,52 @@
 #include<string>
 using namespace std;
 
+int **ms; // reserve a pointer to a 2d array
+int size;
 int loadms (string fn) {
   ifstream myfile;
-  int rownum;
   myfile.open (fn);
   cout << "reading from a file.\n";
-  myfile>>rownum;
-  string arow;
+  myfile>>size;
+  int acell;
+  //two levels of dynamic memory allocation for 2d array
+  ms=new int *[size];
+  for(int i=0; i<size; i++)
+	  ms[i] = new int[size];
 
-  for (int row=0; row<rownum; row++)
-  for (int col=0; col<rownum; col++)
+  for (int row=0; row<size; row++)
+  for (int col=0; col<size; col++)
   {
-	myfile>>arow;
-  	cout<<arow<<endl;
+	myfile>>acell; // read one token a time!
+	ms[row][col]=acell;
   }
+
   myfile.close();
   return 0;
 
 }
+void showms()
+{
 
-int **ms;
+  for (int row=0; row<size; row++)
+  {
+	for (int col=0; col<size; col++)
+  	{
+		cout<<ms[row][col]<<"\t";
+	}
+	cout<<endl;
+  }
 
+}
 
 
 int main()
 {
 
 	string fn;
-	cout<<"Please input file name: " ;
+	cout<<"Please input file name (such as ms1.txt): " ;
 	cin>>fn;
-loadms("ms1.txt"); //fn
-return 0;
+	loadms(fn); //fn
+	showms();
+	return 0;
 }
