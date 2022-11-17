c57d3e16637683dce5fe405a5c5dbbdd8e7f748c
zhangsgithub04 <autodsappt@instance-344.us-central1-a.c.csci2243.internal>
2022-11-17 03:08:37 +0000

    col checker and backward diagnal checker

--- magicsquare/loadms.cpp
+++ magicsquare/loadms.cpp
@@ -5,16 +5,19 @@ using namespace std;
 
 int **ms; // reserve a pointer to a 2d array
 int size;
+
 int loadms (string fn) {
   ifstream myfile;
   myfile.open (fn);
   cout << "reading from a file.\n";
   myfile>>size;
   int acell;
+
   //two levels of dynamic memory allocation for 2d array
   ms=new int *[size];
-  for(int i=0; i<size; i++)
-	  ms[i] = new int[size];
+  for(int row=0; row<size; row++)
+	  ms[row] = new int[size];
+  
 
   for (int row=0; row<size; row++)
   for (int col=0; col<size; col++)
@@ -27,6 +30,7 @@ int loadms (string fn) {
   return 0;
 
 }
+
 void showms()
 {
 
@@ -41,6 +45,32 @@ void showms()
 
 }
 
+int magicnumber;
+bool checkbwdiag() // the backward diagnal /
+{
+	int sum=0;
+	for(int row=0; row<size; row++)
+	{
+		int col=size-1-row; 
+		sum+=ms[row][col];
+	}
+	
+	return (sum==magicnumber);
+}
+
+bool checkacol(int whichcol)
+{
+	int sum=0;
+	for(int row=0; row<size; row++)
+	{
+		sum+=ms[row][whichcol];
+	}
+	
+	return (sum==magicnumber);
+
+}
+
+bool flag=true;
 
 int main()
 {
@@ -50,5 +80,34 @@ int main()
 	cin>>fn;
 	loadms(fn); //fn
 	showms();
+	magicnumber=size*(size*size+1)/2;
+
+	//check backward diagnal
+	if(!checkbwdiag())
+	{
+		cout<<"Not a magic square!"<<endl;
+		flag=false;
+		return -1;
+	}
+
+	//check forward diagnal, your work
+	
+	
+	//check each column
+	for( int col= 0; col<size; col++)
+	{
+		if(!checkacol(col))
+		{
+			cout<<"Not a magic square! " << "detected on col " <<col<<endl;
+			flag=false;
+			return -1; 
+			//break;
+		}
+	}
+
+	//check each row, your work
+
+	cout<<"It is a magic square!"<<endl;
+
 	return 0;
 }
