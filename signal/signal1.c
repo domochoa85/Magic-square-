3274b1f1b5be23ad6491c77b3338a1695879dc49
zhangsgithub04 <autodsappt@instance-344.us-central1-a.c.csci2243.internal>
2022-11-22 15:47:33 +0000

    signal handling in c

---
+++ signal/signal1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include<signal.h>
+#include<unistd.h>
+#include <stdlib.h>
+
+
+
+void sig_handler(int signum){
+
+  //Return type of the handler function should be void
+  printf("\nInside handler function\n");
+  //exit(-1);
+}
+
+int main()
+{
+
+	signal(SIGINT,sig_handler); // Register signal handler
+
+	for(int i=0; i<1000000; i++)
+		printf("%d", i);
+	return 0; 
+}
+
+
