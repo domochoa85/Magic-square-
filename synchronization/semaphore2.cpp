8dc456f68e89672e0d9819f28abae61ab7c83f55
zhangsgithub04 <autodsappt@instance-344.us-central1-a.c.csci2243.internal>
2022-12-08 19:17:27 +0000

    more semaphore

---
+++ synchronization/semaphore2.cpp
@@ -0,0 +1,45 @@
+//g++ semaphore1.cpp -lpthread
+// C program to demonstrate working of Semaphores
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+
+sem_t sem;
+
+void* thread(void* arg)
+{
+	//wait
+	sem_wait(&sem);
+	printf("\nEntered..\n");
+
+	//critical section
+	sleep(4);
+	
+	//signal
+	printf("\nJust Exiting...\n");
+	sem_post(&sem);
+}
+
+
+int main()
+{
+	sem_init(&sem, 0, 3);
+
+	pthread_t t1,t2,t3, t4;
+
+	pthread_create(&t1,NULL,thread,NULL);
+	sleep(2);
+	pthread_create(&t2,NULL,thread,NULL);
+	pthread_create(&t3,NULL,thread,NULL);
+	pthread_create(&t4,NULL,thread,NULL);
+	pthread_join(t1,NULL);
+	pthread_join(t2,NULL);
+	pthread_join(t3,NULL);
+	pthread_join(t4,NULL);
+
+	sem_destroy(&sem);
+	return 0;
+}
+
