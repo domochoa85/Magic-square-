27e0ce364961b18450501082f80799426c66150a
zhangsgithub04 <ssen_zhang@hotmail.com>
2023-10-22 18:25:49 -0400

    Update omp1.c

--- omp1.c
+++ omp1.c
@@ -1,8 +1,10 @@
 // pre-flight commands
 // grep 'processor.*:' /proc/cpuinfo | wc -l
-// export OMP_NUM_THREADS=2
-
-//to compile, gcc -o omp1 -fopenmp thisexample.c
+// export OMP_NUM_THREADS=2 
+// or  4 number returned by the grep command 
+// export OMP_NUM_THREADS=$(grep 'processor.*:' /proc/cpuinfo | wc -l)
+// echo $OMP_NUM_THREADS
+//to compile, gcc -o omp1 -fopenmp omp1.c
 // https://www.linuxtoday.com/blog/openmp/
 
 #include <omp.h> // OpenMP header
