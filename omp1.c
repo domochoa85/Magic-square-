728b5462492d52cc8727feae504e74dcdaecebfc
zhangsgithub04 <ssen_zhang@hotmail.com>
2023-10-22 18:28:54 -0400

    Update omp1.c

--- omp1.c
+++ omp1.c
@@ -1,12 +1,16 @@
-// pre-flight commands
-// grep 'processor.*:' /proc/cpuinfo | wc -l
-// export OMP_NUM_THREADS=2 
-// or  4 number returned by the grep command 
-// export OMP_NUM_THREADS=$(grep 'processor.*:' /proc/cpuinfo | wc -l)
-// echo $OMP_NUM_THREADS
-//to compile, gcc -o omp1 -fopenmp omp1.c
-// https://www.linuxtoday.com/blog/openmp/
-
+/******************
+pre-flight commands to figure out how many cores
+grep 'processor.*:' /proc/cpuinfo | wc -l
+export OMP_NUM_THREADS=2 
+or  4 number returned by the grep command 
+export OMP_NUM_THREADS=$(grep 'processor.*:' /proc/cpuinfo | wc -l)
+echo $OMP_NUM_THREADS
+https://www.linuxtoday.com/blog/openmp/
+The GNU gcc compiler (gcc version 4.7 onwards) already supports OpenMP and compile OpenMP programs by 
+using –fopenmp option, i.e. the command to compile the OpenMP program
+to compile
+gcc -o omp1 -fopenmp omp1.c
+*/
 #include <omp.h> // OpenMP header
 #include <stdio.h>
 #include <stdlib.h>
